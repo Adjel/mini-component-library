67bdcfd838038a105f41cc4a7b20905242fe89d0
Adjel <adjel.touaoula@gmail.com>
2024-02-27 14:29:29 +0100

    progress bar with fonctionnal percentage

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,29 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+    <ProgressWrapper>
+      <ProgressBarProgress style={{ "--value": value + "%" }} />
+    </ProgressWrapper>
+  );
 };
+
+const ProgressWrapper = styled.div`
+  width: 370px;
+  height: 8px;
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: 4px, 0px, 0px, 4px;
+`;
+
+const ProgressBarProgress = styled.div`
+  background-color: ${COLORS.primary};
+  width: var(--value);
+  height: 100%;
+`;
 
 export default ProgressBar;
