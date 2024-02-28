17f96177491d60f1905c5238a6d4bf468f77ec0a
Adjel <adjel.touaoula@gmail.com>
2024-02-28 14:49:48 +0100

    fixed some code

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,7 +5,7 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
-const WRAPPERSIZES = {
+const STYLES = {
   small: {
     height: 8 + "px",
     borderRadius: 4 + "px",
@@ -26,41 +26,46 @@ const WRAPPERSIZES = {
 };
 
 const ProgressBar = ({ value, size }) => {
-  const styleSize = WRAPPERSIZES[size];
-  const isLargeBarRounded = value < 99.6;
+  const styles = STYLES[size];
+
+  if (!styles) {
+    throw new Error(
+      `Unknown sized passed to the progressBar component : ${size}`
+    );
+  }
+
+  const cornersRadius = value < 99.6 ? "4px 0 0 4px" : "4px";
   return (
     <ProgressWrapper
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
       style={{
-        "--borderRadius": styleSize.borderRadius,
-        "--padding": styleSize.padding,
+        "--borderRadius": styles.borderRadius,
+        "--padding": styles.padding,
       }}
     >
       <VisuallyHidden>{value}%</VisuallyHidden>
       <ProgressBarProgress
-        style={
-          !isLargeBarRounded
-            ? {
-                "--value": value + "%",
-                "--height": styleSize.height,
-                "--borderRadius": 4 + "px",
-              }
-            : {
-                "--value": value + "%",
-                "--height": styleSize.height,
-                "--borderRadius": "4px 0 0 4px",
-              }
-        }
+        style={{
+          "--value": value + "%",
+          "--height": styles.height,
+          "--borderRadius": cornersRadius,
+        }}
       />
     </ProgressWrapper>
   );
 };
 
 const ProgressWrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   width: 370px;
   height: var(--height);
   padding: var(--padding);
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
   border-radius: var(--borderRadius);
+  /* trim off corners when progress bar is near-full */
   overflow: hidden;
 `;
 
