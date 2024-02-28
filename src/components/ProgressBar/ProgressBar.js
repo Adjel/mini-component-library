d3acc5f87d7fcb84a4a7a088a34e9db0644aed5b
Adjel <adjel.touaoula@gmail.com>
2024-02-28 14:06:21 +0100

    progressbar done

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,25 +5,70 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
+const WRAPPERSIZES = {
+  small: {
+    height: 8 + "px",
+    borderRadius: 4 + "px",
+    padding: 0 + "px",
+  },
+
+  medium: {
+    height: 12 + "px",
+    borderRadius: 4 + "px",
+    padding: 0 + "px",
+  },
+
+  large: {
+    height: 24 + "px",
+    borderRadius: 8 + "px",
+    padding: 4 + "px",
+  },
+};
+
 const ProgressBar = ({ value, size }) => {
+  const styleSize = WRAPPERSIZES[size];
+  const isLargeBarRounded = value < 99.6;
   return (
-    <ProgressWrapper>
-      <ProgressBarProgress style={{ "--value": value + "%" }} />
+    <ProgressWrapper
+      style={{
+        "--borderRadius": styleSize.borderRadius,
+        "--padding": styleSize.padding,
+      }}
+    >
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <ProgressBarProgress
+        style={
+          !isLargeBarRounded
+            ? {
+                "--value": value + "%",
+                "--height": styleSize.height,
+                "--borderRadius": 4 + "px",
+              }
+            : {
+                "--value": value + "%",
+                "--height": styleSize.height,
+                "--borderRadius": "4px 0 0 4px",
+              }
+        }
+      />
     </ProgressWrapper>
   );
 };
 
 const ProgressWrapper = styled.div`
   width: 370px;
-  height: 8px;
+  height: var(--height);
+  padding: var(--padding);
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-  border-radius: 4px, 0px, 0px, 4px;
+  border-radius: var(--borderRadius);
+  overflow: hidden;
 `;
 
 const ProgressBarProgress = styled.div`
   background-color: ${COLORS.primary};
   width: var(--value);
-  height: 100%;
+  height: var(--height);
+  border-radius: var(--borderRadius);
 `;
 
 export default ProgressBar;
